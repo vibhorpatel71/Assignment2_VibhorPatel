d541f35e3128f953629ee36003b58c6c9c59d92e
vibhorpatel71 <156718864+vibhorpatel71@users.noreply.github.com>
2024-12-05 20:21:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello, Dockerized Node.js App!');
+	res.send('Hello From Vibhor, Dockerized Node.js App! Thanks for visiting');
 });
 
 app.listen(PORT, () => {
